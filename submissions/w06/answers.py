99fb987e2042ee8bfb8dca32e5a9c0dc374d4459
ark <hexagononyt@gmail.com>
2026-04-13 14:53:30 +0700

    W06 submission

--- submissions/w06/answers.py
+++ submissions/w06/answers.py
@@ -13,15 +13,15 @@ from __future__ import annotations
 def q01() -> bool:
     """[T/F] Distribusi Binomial digunakan untuk eksperimen dengan jumlah percobaan yang
 tidak terbatas."""
-    raise NotImplementedError
+    return False
 
 def q02() -> bool:
     """[T/F] Parameter mean dan variansi pada distribusi Poisson memiliki nilai yang sama."""
-    raise NotImplementedError
+    return True
 
 def q03() -> bool:
     """[T/F] Distribusi Bernoulli adalah kasus khusus dari distribusi Binomial dengan n = 1."""
-    raise NotImplementedError
+    return True
 
 def q04() -> str:
     """[MC] Jika X Bn(10,0,2), maka nilai harapannya adalah:
@@ -30,7 +30,7 @@ A) 2
 B) 0,2
 C) 8
 D) 1,6"""
-    raise NotImplementedError
+    return 'A'
 
 def q05() -> str:
     """[MC] Distribusi yang paling tepat untuk memodelkan jumlah telepon yang masuk ke call
@@ -40,7 +40,7 @@ A) Binomial.
 B) Poisson.
 C) Uniform.
 D) Normal."""
-    raise NotImplementedError
+    return 'B'
 
 def q06() -> str:
     """[MC] Pada distribusi Binomial, probabilitas sukses p harus:
@@ -49,7 +49,7 @@ A) Berubah tiap percobaan.
 B) Tetap konstan tiap percobaan.
 C) Selalu 0,5.
 D) Berkurang seiring waktu."""
-    raise NotImplementedError
+    return 'B'
 
 def q07() -> str:
     """[MC] Rumus P(X = ) = − adalah untuk distribusi:
@@ -58,27 +58,27 @@ A) Binomial.
 B) Poisson.
 C) Geometrik.
 D) Eksponensial."""
-    raise NotImplementedError
+    return 'B'
 
 def q08() -> float:
     """[Numeric] Jika X Bn(4,0,5), hitung P(X = 2)."""
-    raise NotImplementedError
+    return 0.375
 
 def q09() -> float:
     """[Numeric] Untuk distribusi Poisson dengan = 2, berapakah probabilitas P(X = 0)?
 (Gunakan 3 desimal)"""
-    raise NotImplementedError
+    return 0.135
 
 def q10() -> float:
     """[Numeric] Hitung variansi dari variabel acak X Bn(100,0,1)."""
-    raise NotImplementedError
+    return 9.0
 
 def q11() -> float:
     """[Numeric] Berapakah nilai maksimum yang mungkin dari variabel acak X Bn(10,0,5)?"""
-    raise NotImplementedError
+    return 10.0
 
 def q12() -> float:
     """[Numeric] Jika rata-rata kedatangan paket adalah 5 per ms, berapakah variansi jumlah
 paket per ms?"""
-    raise NotImplementedError
+    return 5.0
 
