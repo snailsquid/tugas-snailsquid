f8cf1fadf075285eb27f1552352d44a8d7801044
ark <hexagononyt@gmail.com>
2026-04-13 14:53:21 +0700

    W03 submission

--- submissions/w03/answers.py
+++ submissions/w03/answers.py
@@ -12,17 +12,17 @@ Format jawaban:
 from __future__ import annotations
 def q01() -> bool:
     """[T/F] Dalam konfigurasi paralel, sistem akan gagal hanya jika semua komponen gagal."""
-    raise NotImplementedError
+    return True
 
 def q02() -> bool:
     """[T/F] Menambahkan komponen secara seri akan meningkatkan reliabilitas keseluruhan
 sistem."""
-    raise NotImplementedError
+    return False
 
 def q03() -> bool:
     """[T/F] Reliabilitas total sistem seri selalu lebih kecil atau sama dengan reliabilitas
 komponen terlemahnya."""
-    raise NotImplementedError
+    return True
 
 def q04() -> str:
     """[MC] Sebuah sistem terdiri dari dua komponen dengan reliabilitas 0,9 yang disusun
@@ -32,7 +32,7 @@ A) 0,81
 B) 0,99
 C) 0,90
 D) 1,80"""
-    raise NotImplementedError
+    return 'B'
 
 def q05() -> str:
     """[MC] Jika tiga switch identik dengan reliabilitas 0,8 disusun seri, reliabilitas totalnya
@@ -42,7 +42,7 @@ A) 0,512
 B) 0,8
 C) 2,4
 D) 0,2"""
-    raise NotImplementedError
+    return 'A'
 
 def q06() -> str:
     """[MC] Manakah konfigurasi yang paling tahan terhadap kegagalan komponen tunggal?
@@ -51,7 +51,7 @@ A) Seri.
 B) Paralel.
 C) Campuran seri-paralel.
 D) Sistem tanpa redundansi."""
-    raise NotImplementedError
+    return 'B'
 
 def q07() -> str:
     """[MC] Istilah untuk probabilitas sistem berfungsi pada waktu tertentu t adalah:
@@ -60,29 +60,29 @@ A) Efisiensi.
 B) Reliabilitas.
 C) Kapasitas.
 D) Latensi."""
-    raise NotImplementedError
+    return 'B'
 
 def q08() -> float:
     """[Numeric] Hitung reliabilitas sistem seri dari dua komponen dengan reliabilitas 0,95 dan
 0,8."""
-    raise NotImplementedError
+    return 0.76
 
 def q09() -> float:
     """[Numeric] Jika reliabilitas satu server adalah 0,9, berapa probabilitas dua server
 tersebut gagal bersamaan dalam susunan paralel?"""
-    raise NotImplementedError
+    return 0.01
 
 def q10() -> float:
     """[Numeric] Sebuah sistem memiliki reliabilitas 0,99. Berapa probabilitas kegagalannya?"""
-    raise NotImplementedError
+    return 0.01
 
 def q11() -> float:
     """[Numeric] Tiga lampu disusun paralel dengan reliabilitas masing-masing 0,5. Berapa
 reliabilitas total sistem lampu tersebut?"""
-    raise NotImplementedError
+    return 0.875
 
 def q12() -> float:
     """[Numeric] Berapa probabilitas sistem seri dengan 10 komponen identik (masing-masing
 R = 0,99) tetap berfungsi? (Gunakan 3 desimal)"""
-    raise NotImplementedError
+    return 0.904
 
